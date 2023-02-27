ae712f784f2a8829973abded327911430cf4e431
Saivelin <vlad.pochkalov@mail.ru>
2023-02-28 00:10:16 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import React from 'react';
+import Moment from 'react-moment';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App" style={{ background: "url('/background-day.jpg')", backgroundSize: "cover" }}>
+            <div className='clockAndMoreItem'>
+                <Moment format='HH:mm' interval={1000} />
+            </div>
+            <div className='mainWhether'>
+                <div className='mainWhether__item'>
+                    <h2 className='mainWhether__item-header'>-25*</h2>
+                    <p className='mainWhether__item-about'>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem </p>
+                </div>
+                <div className='mainWhether__item'>
+                    <h2 className='mainWhether__item-header'>-25*</h2>
+                    <p className='mainWhether__item-about'>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem </p>
+                </div>
+                <div className='mainWhether__item'>
+                    <h2 className='mainWhether__item-header'>-25*</h2>
+                    <p className='mainWhether__item-about'>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem </p>
+                </div>
+                <div className='mainWhether__item'>
+                    <h2 className='mainWhether__item-header'>-25*</h2>
+                    <p className='mainWhether__item-about'>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem </p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
